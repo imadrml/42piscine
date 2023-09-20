b53c70592408a4237a65597360b514a8b3abd219
imadrml <102284276+imadrml@users.noreply.github.com>
2023-09-20 16:45:42 -0400

    Add files via upload

--- c02/ex00/ft_strcpy.c
+++ c02/ex00/ft_strcpy.c
@@ -24,3 +24,17 @@ char	*ft_strcpy(char *dest, char *src)
 	dest[i] = '\0';
 	return (dest);
 }
+/*int main(void)
+{
+    char source[] = "Hello, World!";
+    char destination[20]; // Define a destination buffer
+
+    
+    ft_strcpy(destination, source);
+
+    // Print the result to verify
+    printf("Source: %s\n", source);
+    printf("Destination: %s\n", destination);
+
+    return 0;
+}*/
